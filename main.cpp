db34fb6768c08e2c222bd670952d6f05e873be00
Navigator <emosin2007@gmail.com>
2023-04-23 20:00:48 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include "inout.h"
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    vector<int> vec;
+    read(vec);
+    sort(vec.begin(), vec.end());
+    cout << endl << endl;
+    print(vec);
+    return 0;
+}
